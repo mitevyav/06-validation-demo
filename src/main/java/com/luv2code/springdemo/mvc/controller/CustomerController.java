3484037b6a36c1445f8985cd5f99eb860c192363
yavor <jm87585@gmail.com>
2023-09-22 17:45:25 +0300

    Use @min and @max for validation

--- src/main/java/com/luv2code/springdemo/mvc/controller/CustomerController.java
+++ src/main/java/com/luv2code/springdemo/mvc/controller/CustomerController.java
@@ -2,15 +2,26 @@ package com.luv2code.springdemo.mvc.controller;
 
 import com.luv2code.springdemo.mvc.entity.Customer;
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+
+
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        StringTrimmerEditor editor = new StringTrimmerEditor(true);
+
+        dataBinder.registerCustomEditor(String.class, editor);
+    }
 
     @GetMapping("/")
     public String showForm(Model model) {
